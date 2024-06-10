be6cdc87d9d086250fd34a1c6a83f5c03f00211d
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-06-10 22:40:53 +0900

    final!

--- drive.py
+++ drive.py
@@ -6,6 +6,7 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 
+# 좌우 모터 및 핀 설정
 PWMA = 18
 AIN1 = 22
 AIN2 = 27
@@ -14,145 +15,151 @@ PWMB = 23
 BIN1 = 25
 BIN2 = 24
 
+# 모터 전진 함수
 def motor_go(speed):
-    L_Motor.ChangeDutyCycle(speed)
-    GPIO.output(AIN2, True)
+    L_Motor.ChangeDutyCycle(speed)  # 왼쪽 모터 속도 설정
+    GPIO.output(AIN2, True)  # 왼쪽 모터 방향 설정
     GPIO.output(AIN1, False)
-    R_Motor.ChangeDutyCycle(speed)
-    GPIO.output(BIN2, True)
+    R_Motor.ChangeDutyCycle(speed)  # 오른쪽 모터 속도 설정
+    GPIO.output(BIN2, True)  # 오른쪽 모터 방향 설정
     GPIO.output(BIN1, False)
 
+# 모터 정지 함수
 def motor_stop():
-    L_Motor.ChangeDutyCycle(0)
+    L_Motor.ChangeDutyCycle(0)  # 왼쪽 모터 정지
     GPIO.output(AIN2, False)
     GPIO.output(AIN1, False)
-    R_Motor.ChangeDutyCycle(0)
+    R_Motor.ChangeDutyCycle(0)  # 오른쪽 모터 정지
     GPIO.output(BIN2, False)
     GPIO.output(BIN1, False)
 
+# 모터 우회전 함수
 def motor_right(speed):
-    L_Motor.ChangeDutyCycle(speed)
-    GPIO.output(AIN2, True)
+    L_Motor.ChangeDutyCycle(speed)  # 왼쪽 모터 속도 설정
+    GPIO.output(AIN2, True)  # 왼쪽 모터 방향 설정
     GPIO.output(AIN1, False)
-    R_Motor.ChangeDutyCycle(0)
+    R_Motor.ChangeDutyCycle(0)  # 오른쪽 모터 정지
     GPIO.output(BIN2, False)
     GPIO.output(BIN1, True)
 
+# 모터 좌회전 함수
 def motor_left(speed):
-    L_Motor.ChangeDutyCycle(0)
+    L_Motor.ChangeDutyCycle(0)  # 왼쪽 모터 정지
     GPIO.output(AIN2, False)
     GPIO.output(AIN1, True)
-    R_Motor.ChangeDutyCycle(speed)
-    GPIO.output(BIN2, True)
+    R_Motor.ChangeDutyCycle(speed)  # 오른쪽 모터 속도 설정
+    GPIO.output(BIN2, True)  # 오른쪽 모터 방향 설정
     GPIO.output(BIN1, False)
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(AIN2, GPIO.OUT)
+GPIO.setwarnings(False)  # GPIO 경고 무시
+GPIO.setmode(GPIO.BCM)  # GPIO 핀 번호를 BCM 모드로 설정
+GPIO.setup(AIN2, GPIO.OUT)  # 핀을 출력으로 설정
 GPIO.setup(AIN1, GPIO.OUT)
 GPIO.setup(PWMA, GPIO.OUT)
 GPIO.setup(BIN1, GPIO.OUT)
 GPIO.setup(BIN2, GPIO.OUT)
 GPIO.setup(PWMB, GPIO.OUT)
 
-L_Motor = GPIO.PWM(PWMA, 100)
-L_Motor.start(0)
-R_Motor = GPIO.PWM(PWMB, 100)
-R_Motor.start(0)
+L_Motor = GPIO.PWM(PWMA, 100)  # 왼쪽 모터를 100Hz의 PWM으로 설정
+L_Motor.start(0)  # 왼쪽 모터를 0의 듀티 사이클로 시작
+R_Motor = GPIO.PWM(PWMB, 100)  # 오른쪽 모터를 100Hz의 PWM으로 설정
+R_Motor.start(0)  # 오른쪽 모터를 0의 듀티 사이클로 시작
 
+# 이미지 전처리 함수 (차선 검출용)
 def img_preprocess(image):
-    height, _, _ = image.shape
-    image = image[int(height / 2):, :, :]
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
-    image = cv2.resize(image, (220, 70))
-    image = cv2.GaussianBlur(image, (5, 5), 0)
-    _, image = cv2.threshold(image, 140, 255, cv2.THRESH_BINARY_INV)
+    height, _, _ = image.shape  # 이미지의 높이를 구하기
+    image = image[int(height / 2):, :, :]  # 이미지의 하단 절반을 잘라냄
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)  # 이미지를 YUV 색상 공간으로 변환
+    image = cv2.resize(image, (220, 70))  # 이미지 크기 변경
+    image = cv2.GaussianBlur(image, (5, 5), 0)  # 가우시안 블러 적용
+    _, image = cv2.threshold(image, 140, 255, cv2.THRESH_BINARY_INV)  # 이진화 수행
     return image
 
+# 이미지 전처리 함수 (정지선 검출용)
 def stopline_img_preprocess(image):
-    height, _, _ = image.shape
-    image = image[int(height / 2):, :, :]
-    image = cv2.resize(image, (70,220))
-    image = cv2.GaussianBlur(image, (5, 5), 0)
-    _, image = cv2.threshold(image,140, 255, cv2.THRESH_BINARY_INV)        # 임계값 140이었음, 180이 일단 베스트
-    image = img_to_array(image)
-    image = image / 255.0
-    image = np.expand_dims(image, axis=0)  # Add batch dimension
+    height, _, _ = image.shape  # 이미지의 높이를 구하기
+    image = image[int(height / 2):, :, :]  # 이미지의 하단 절반을 잘라냄
+    image = cv2.resize(image, (70, 220))  # 이미지 크기 변경
+    image = cv2.GaussianBlur(image, (5, 5), 0)  # 가우시안 블러 적용
+    _, image = cv2.threshold(image, 140, 255, cv2.THRESH_BINARY_INV)  # 이진화 수행
+    image = img_to_array(image)  # 이미지를 배열로 변환
+    image = image / 255.0  # 이미지 정규화
+    image = np.expand_dims(image, axis=0)  # 배치 차원 추가
     return image
 
-stopline_flag = False
+stopline_flag = False  # 정지선 감지 플래그 초기화
 
+# 정지선 감지 플래그 재설정 함수
 def reset_stopline_flag():
     global stopline_flag
     stopline_flag = False
 
-camera = cv2.VideoCapture(0)
-camera.set(3, 640)
+camera = cv2.VideoCapture(0)  # 카메라를 열기
+camera.set(3, 640)  # 카메라 해상도를 설정
 camera.set(4, 480)
 
-carState = "stop"
-frame = None
-lock = threading.Lock()
+carState = "stop"  # 자동차 상태를 정지로 초기화
+frame = None  # 프레임 초기화
+lock = threading.Lock()  # 스레드 동기화를 위한 Lock 객체를 생성
 
+# 카메라 프레임 캡처 함수
 def capture_frames():
     global frame
     while True:
-        ret, image = camera.read()
+        ret, image = camera.read()  # 프레임을 캡처
         if not ret:
             continue
-        image = cv2.flip(image, -1)
+        image = cv2.flip(image, -1)  # 이미지를 수평으로 뒤집기
         with lock:
-            frame = image
-        #time.sleep(0.01)  # 약간의 지연을 추가하여 CPU 사용률을 낮춤
+            frame = image  # 프레임을 업데이트
 
+# 프레임 처리 함수 (차선 및 정지선 검출)
 def process_frames():
     global carState, frame
-    model_path = './model/lane_navigation_model.h5'   # 이거만 바꾸면 됨
-    stopline_model_path = './model/stopline_model.h5'
+    model_path = './model/lane_navigation_model.h5'  # 차선 인식 모델 경로
+    stopline_model_path = './model/stopline_model.h5'  # 정지선 인식 모델 경로
     
-    model = load_model(model_path)
-    stopline_model = load_model(stopline_model_path)
+    model = load_model(model_path)  # 차선 인식 모델 로드
+    stopline_model = load_model(stopline_model_path)  # 정지선 인식 모델 로드
 
     try:
         while True:
             with lock:
                 if frame is None:
                     continue
-                preprocessed = img_preprocess(frame)
-                stopline_X = stopline_img_preprocess(frame)
+                preprocessed = img_preprocess(frame)  # 차선 검출을 위해 프레임 전처리
+                stopline_X = stopline_img_preprocess(frame)  # 정지선 검출을 위해 프레임 전처리
 
-            cv2.imshow('pre', preprocessed)
+            cv2.imshow('pre', preprocessed)  # 전처리된 이미지를 화면에 표시
 
-            # 조향각 예측
+            # 조향 각도 예측
             preprocessed = img_to_array(preprocessed)
             preprocessed = preprocessed / 255.0
-
             X = np.asarray([preprocessed])
-
             prediction = model.predict(X)
-            steering_angle = prediction[0][0]
+            steering_angle = prediction[0][0]   # 예측된 조향각
             print("Predicted angle:", steering_angle)
 
-            # 정지선 예측
+            # 정지선 검출
             stopline_prediction = stopline_model.predict(stopline_X)
-            # stopline_detected = stopline_prediction[0][0] > 0.5  # 예측값이 0.5 이상이면 정지선으로 간주
-            stopline_detected = np.argmax(stopline_prediction[0])
-            
-            global stopline_flag
+            stopline_detected = np.argmax(stopline_prediction[0])   # 정지선 검출시 1, 미검출시 0
+            global stopline_flag    # 함수 밖에서 정의한 정지선 검출 플래그 불러오기
 
+            # 정지 가능 상태 & 정지선이 검출된 경우
             if stopline_detected and not stopline_flag:
                 print("Stopline detected, stopping for 3 seconds")
                 motor_stop()
                 time.sleep(3)  # 3초 동안 정지
                 stopline_flag = True
 
-                # stopline_flag를 True로 설정한 후 5초 후에 False로 다시 설정하는 타이머 시작
+                # stopline_flag를 True로 설정한 후 10초 후에 다시 False로 설정
+                # 10초 동안만 정지 불가능한 상태로 만드는 것
                 threading.Timer(10, reset_stopline_flag).start()
 
                 continue
 
             if carState == "go":
-                if 70 <= steering_angle <= 100:     # 70, 100이었음
+                if 70 <= steering_angle <= 100:  # 조향 각도에 따라 직진, 우회전, 좌회전을 결정
                     print("go")
                     speedSet = 40
                     motor_go(speedSet)
@@ -162,7 +169,7 @@ def process_frames():
                     motor_right(speedSet)
                 elif steering_angle < 70:
                     print("left")
-                    speedSet = 38   #35
+                    speedSet = 38
                     motor_left(speedSet)
             elif carState == "stop":
                 motor_stop()
@@ -180,16 +187,17 @@ def process_frames():
     except KeyboardInterrupt:
         pass
 
+# 메인 함수
 def main():
-    capture_thread = threading.Thread(target=capture_frames)
-    process_thread = threading.Thread(target=process_frames)
+    capture_thread = threading.Thread(target=capture_frames)  # 프레임 캡처용 스레드 생성
+    process_thread = threading.Thread(target=process_frames)  # 프레임 처리용 스레드 생성
 
-    capture_thread.start()
-    process_thread.start()
+    capture_thread.start()  # 프레임 캡처 스레드 시작
+    process_thread.start()  # 프레임 처리 스레드 시작
 
-    capture_thread.join()
-    process_thread.join()
+    capture_thread.join()  # 프레임 캡처 스레드가 종료될 때까지 대기
+    process_thread.join()  # 프레임 처리 스레드가 종료될 때까지 대기
 
 if __name__ == '__main__':
-    main()
-    GPIO.cleanup()
+    main()  # 메인 함수 실행
+    GPIO.cleanup()  # GPIO 리소스 정리
